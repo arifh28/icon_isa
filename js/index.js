488d026d8b574cb7c0c8757e14dce4bccc3c07ab
arifh28 <arif.hidayat28@gmail.com>
2016-02-21 00:28:48 +0700

    v1 insyaAllah

--- js/index.js
+++ js/index.js
@@ -5,5 +5,31 @@ $(document).ready(function($) {
    		$(this).parent().find( 'div' ).toggleClass( 'card__social--active' );
 		$(this).toggleClass('share-expanded');
     });
+
+        var $mq = $('.marquee2, .marquee3').marquee({
+        pauseOnHover: true,
+        direction: 'right'
+    });
+
+    //Pause
+    $('.t').click(function(){
+        $mq.marquee('toggle');
+    });
+
+    setInterval(function(){
+        $('.marquee2').marquee('toggle');
+    },700);
+    $('.marquee').marquee({
+        //speed in milliseconds of the marquee
+        duration: 10000,
+        pauseOnHover: true,
+        //time in milliseconds before the marquee will start animating
+        delayBeforeStart: 0,
+        //'left' or 'right'
+        direction: 'right',
+        //true or false - should the marquee be duplicated to show an effect of continues flow
+        duplicated: true
+    });
   
 });
+
